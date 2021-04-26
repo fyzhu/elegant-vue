21a2c79cd5b04deaa3d13879fc5966d997bcd5e4
YvanZhu <i@zhuyunfeng.com>
2021-04-26 10:27:02 +0800

    feat 更新部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 rm -rf dist &&
 yarn build &&
-cd dist &&
-git init &&
-git add . &&
-git commit -m "deploy" &&
-git branch -M master &&
-git remote add origin git@github.com:fyzhu/elegant-vue.git &&
-git push -f -u origin master &&
-cd -
+scp -r ./dist/* root@dreamlist.cn:/www/wwwroot/elegant.dreamlist.cn/
+# cd dist &&
+# git init &&
+# git add . &&
+# git commit -m "deploy" &&
+# git branch -M master &&
+# git remote add origin git@github.com:fyzhu/elegant-vue.git &&
+# git push -f -u origin master &&
+# cd -
